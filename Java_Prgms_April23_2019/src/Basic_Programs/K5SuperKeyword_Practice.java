a99bf92389dc0ad1110fd3958de49b5cd3e69691
Pallavi.N <Pallavi.N@AURBLRLT-206.aurigoblr.com>
2019-08-10 17:24:49 +0530

    pal

--- Java_Prgms_April23_2019/src/Basic_Programs/K5SuperKeyword_Practice.java
+++ Java_Prgms_April23_2019/src/Basic_Programs/K5SuperKeyword_Practice.java
@@ -4,11 +4,11 @@ class Acon2
 {
 	Acon2()
 	{
-		System.out.println("AAAAAAA cons");
+		System.out.println("AAAAAAA cons");//1
 	}
 	void bujji(int n)
 	{
-		System.out.println("AAAA int"+n);
+		System.out.println("AAAA int"+n);//3
 	}
 }
 
@@ -17,12 +17,12 @@ class Bcon2 extends Acon2
 {
 	Bcon2()
 	{
-		System.out.println("BBB cons");
+		System.out.println("BBB cons");//2
 	}
 	void Baby(int n)
 	{
 		super.bujji(80);
-		System.out.println("BBBB int"+n);
+		System.out.println("BBBB int"+n);//4
 	}
 }
 
